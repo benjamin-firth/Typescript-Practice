28ee4807fbfc4ceb98738088104dc4f43f63ab93
Ben Firth <bfirth93@gmail.com>
2020-09-16 11:47:55 -0600

    Add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 interface IAppOwnProps {
@@ -7,10 +7,15 @@ interface IAppOwnProps {
 }
 
 const App: React.FC<IAppOwnProps> = ({ userType, username }): JSX.Element => {
+  const [time, setTime] = useState<Date>(() => new Date(Date.now()));
+
   return (
     <div className="App">
       <p>
         Hi, {username ? username : 'Mysterious Entity'}, your user type is {username ? userType : 'irrelevant because I do not know you'}.
+      </p>
+      <p>
+        {time.toUTCString()}
       </p>
     </div>
   );
